4f99e022a8f90006312c510c9b4ec11cf75b4044
Julien Caillon <julien.caillon@gmail.com>
2018-03-31 15:00:04 +0200

    Refactoring into a project to use with git submodule

---
+++ Controls/IScrollableControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YamuiFramework.Controls {
+
+    interface IScrollableControl {
+
+        /// <summary>
+        /// Should call the protected method UpdateBounds which... updates bounds
+        /// </summary>
+        void UpdateBoundsPublic();
+
+    }
+}
